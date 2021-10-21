52b85fc3545c9cb7509a8748d0d89cfef7f3919f
Diego Mijangos <diegisseb@gmail.com>
2021-10-21 09:49:34 -0600

    nuevo commitdsmf45

--- src/PaginaProductos.js
+++ src/PaginaProductos.js
@@ -10,9 +10,9 @@ class Apptres extends Component {
       <div className="App">
         <nav class="page__menu page__custom-settings menu">
           <ul class="menu__list r-list">
-            <li class="menu__group"><Link to="/" class="menu__link r-link text-underlined">Pagina 1</Link></li>
-            <li class="menu__group"><Link to="/PaginaProductos" class="menu__link r-link text-underlined">Pagina 2</Link></li>
-            <li class="menu__group"><Link to="/PaginaDeConctacto" class="menu__link r-link text-underlined">Pagina 3</Link></li>
+            <li class="menu__group"><Link to="/" class="menu__link r-link text-underlined">Página principal</Link></li>
+            <li class="menu__group"><Link to="/PaginaProductos" class="menu__link r-link text-underlined">Información de productos</Link></li>
+            <li class="menu__group"><Link to="/PaginaDeConctacto" class="menu__link r-link text-underlined">Contacto</Link></li>
           </ul>
         </nav>
         <header className="App-header">
