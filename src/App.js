a41a465e4bc87b5b0c28feaf6b133473b28bcadf
Diego Mijangos <diegisseb@gmail.com>
2021-10-20 22:46:00 -0600

    cm5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link } from "react-router-dom";
 
-class Appdos extends Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Curso de Sistemas Operativos</h1>
+          <h1 className="App-title">Sdes</h1>
         </header>
         <p className="App-intro">
-          Seccion de que trata el curso
+          Estas son las sedes
 
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Datos de contacto</h1>
+        </header>
+        <p className="App-intro">
+          Datos de contacto
+
+        </p>
+
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Video informativo</h1>
+        </header>
+        <p className="App-intro">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/tN6-KziOBzg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+
+        </p>
+        <br />
+        <button href="PaginaDeConctacto.js" >Siguiente pàgina</button >
+
+
       </div>
     );
   }
 }
 
-export default Appdos;
+export default App;
