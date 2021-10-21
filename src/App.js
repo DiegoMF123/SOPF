4390fcd628d51af826ccb933685c0a82d3c72a50
Diego Mijangos <diegisseb@gmail.com>
2021-10-20 21:35:28 -0600

    p3

--- src/App.js
+++ src/App.js
@@ -11,27 +11,45 @@ class App extends Component {
           <h1 className="App-title">Visiòn</h1>
         </header>
         <p className="App-intro">
-          Seccion para la visiòn
+          Constituir un equipo dentro de nuestra empresa con espíritu emprendedor, con actitud de servicio, ofreciendo servicios y productos   innovadores   con calidad y aun precio accesible, además de hacerle pasar al cliente en su estancia un ambiente seguro en compañía de la mejor música.
 
         </p>
-        
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Misiòn</h1>
         </header>
         <p className="App-intro">
-          Seccion para la misiòn
+          Ser reconocidos como una empresa innovadora, moderna y que satisface a una necesidad de nuestros clientes por el servicio que brindamos, además de la calidad en nuestros
 
         </p>
-        
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Objetivos</h1>
         </header>
         <p className="App-intro">
-          Seccion para los objetivos
+          <li>
+            Mantener el liderazgo
+          </li>
+          <li>
+            Ofrecer excelente servicio
+          </li>
+          <li>
+            Ser rentable
+          </li>
+          <li>
+            Contribuir con el desarrollo del país.
+          </li>
+          <li>
+            Ampliar la red de distribución en un 35% durante el último mes del año.
+          </li>
+
 
         </p>
+        <br/>
+        <button href="PaginaDeConctacto.js" >Siguiente pàgina</button >
+
       </div>
     );
   }
