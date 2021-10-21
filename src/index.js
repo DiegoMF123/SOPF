0b25471999ecbac869396aef7e3bea226549ba44
Diego Mijangos <diegisseb@gmail.com>
2021-10-20 20:49:18 -0600

    treavo commit

--- src/index.js
+++ src/index.js
@@ -7,5 +7,4 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-<script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
 registerServiceWorker();
