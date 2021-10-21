af43a376eadc95710361b7489fcd83dda284cb2e
Diego Mijangos <diegisseb@gmail.com>
2021-10-20 22:28:13 -0600

    com4

--- src/App.js
+++ src/App.js
@@ -1,60 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Link } from "react-router-dom";
 
-class App extends Component {
+class Appdos extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Visiòn</h1>
+          <h1 className="App-title">Curso de Sistemas Operativos</h1>
         </header>
         <p className="App-intro">
-          Constituir un equipo dentro de nuestra empresa con espíritu emprendedor, con actitud de servicio, ofreciendo servicios y productos   innovadores   con calidad y aun precio accesible, además de hacerle pasar al cliente en su estancia un ambiente seguro en compañía de la mejor música.
+          Seccion de que trata el curso
 
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Misiòn</h1>
-        </header>
-        <p className="App-intro">
-          Ser reconocidos como una empresa innovadora, moderna y que satisface a una necesidad de nuestros clientes por el servicio que brindamos, además de la calidad en nuestros
-
-        </p>
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Objetivos</h1>
-        </header>
-        <p className="App-intro">
-          <li>
-            Mantener el liderazgo
-          </li>
-          <li>
-            Ofrecer excelente servicio
-          </li>
-          <li>
-            Ser rentable
-          </li>
-          <li>
-            Contribuir con el desarrollo del país.
-          </li>
-          <li>
-            Ampliar la red de distribución en un 35% durante el último mes del año.
-          </li>
-
-
-        </p>
-        <br/>
-        <button href="PaginaDeConctacto.js" >Siguiente pàgina</button >
-        
-
       </div>
     );
   }
 }
 
-export default App;
+export default Appdos;
