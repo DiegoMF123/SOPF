3f7728687bc6c6e17d26cb15037a49121799afd4
Diego Mijangos <diegisseb@gmail.com>
2021-10-20 21:52:13 -0600

    commit

--- src/PaginaDeConctacto.js
+++ src/PaginaDeConctacto.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Appdos extends Component {
   render() {
     return (
       <div className="App">
@@ -20,4 +20,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Appdos;
