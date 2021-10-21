d806d5ec39dad4a7235468c609d36659f2f48d4b
Diego Mijangos <diegisseb@gmail.com>
2021-10-20 23:27:58 -0600

    dsadasd2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 
 class App extends Component {
   render() {
