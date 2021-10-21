cec5fbf285ef06d947ee825498a3928bad76d5b2
Diego Mijangos <diegisseb@gmail.com>
2021-10-20 22:09:58 -0600

    commit3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Link } from "react-router-dom";
+import {Link } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -48,12 +48,9 @@ class App extends Component {
 
 
         </p>
-        <br />
+        <br/>
         <button href="PaginaDeConctacto.js" >Siguiente pàgina</button >
-        <Link to="/PaginaDeConctacto"><button>
-          Siguiente pàgina
-        </button>
-        </Link>
+        
 
       </div>
     );
