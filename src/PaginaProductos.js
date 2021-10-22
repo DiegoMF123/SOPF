10f3f9b130c7b7048eb50f6b241105fb9c82287a
Diego Mijangos <diegisseb@gmail.com>
2021-10-21 22:21:21 -0600

    Hs2223

--- src/PaginaProductos.js
+++ src/PaginaProductos.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import img from './1.jpg';
+import img2 from './2.jpg';
+import img3 from './3.jpg';
+import img4 from './4.jpg';
+import img5 from './5.jpg';
+import img6 from './6.jpg';
+import img7 from './7.jpg';
+import img8 from './8.jpg';
+import img9 from './9.jpg';
+import img10 from './10.jpg';
 import './App.css';
 import './style.css';
 
@@ -29,18 +39,22 @@ class Apptres extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <br />
-        <div class="content-carrousel">
-            <figure><img src={logo} alt="logo" /></figure>
-            <figure><img src={logo} alt="logo" /></figure>
-            <figure><img src={logo} alt="logo" /></figure>
-            <figure><img src={logo} alt="logo" /></figure>
-            <figure><img src={logo} alt="logo" /></figure>
-            <figure><img src={logo} alt="logo" /></figure>
-            <figure><img src={logo} alt="logo" /></figure>
-            <figure><img src={logo} alt="logo" /></figure>
-            <figure><img src={logo} alt="logo" /></figure>
-            <figure><img src={logo} alt="logo" /></figure>
+        <div className="Appdos">
+          <div class="content-carrousel">
+            <figure><img src={img} alt="logo" /></figure>
+            <figure><img src={img2} alt="logo" /></figure>
+            <figure><img src={img3} alt="logo" /></figure>
+            <figure><img src={img5} alt="logo" /></figure>
+            <figure><img src={img4} alt="logo" /></figure>
+            <figure><img src={img6} alt="logo" /></figure>
+            <figure><img src={img7} alt="logo" /></figure>
+            <figure><img src={img8} alt="logo" /></figure>
+            <figure><img src={img9} alt="logo" /></figure>
+            <figure><img src={img10} alt="logo" /></figure>
+          </div>
         </div>
+        <br />
+        <br />
         <br />
         <Link to="/PaginaDeConctacto"><button class="btn btn-info">
           Siguiente pàgina
