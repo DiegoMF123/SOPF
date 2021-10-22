58d06e6da4a113338116bc562ed7980729237500
Diego Mijangos <diegisseb@gmail.com>
2021-10-22 00:04:19 -0600

    Hs22Sdds2ddb

--- src/PaginaDeConctacto.js
+++ src/PaginaDeConctacto.js
@@ -20,16 +20,20 @@ class Appdos extends Component {
           <h1 className="App-title">Sedes</h1>
         </header>
         <p className="App-intro">
-          Estas son las sedes
+          Ubicaciòn de nuestra sede
 
         </p>
         <br />
         <br />
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.78728754968!2d-90.45828788531881!3d14.668009579353928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x858999096fdbd161%3A0x170e7fbdd215149!2sGuatemala!5e0!3m2!1ses!2sgt!4v1634882041863!5m2!1ses!2sgt"
-          width="600" height="450" allowfullscreen="" loading="lazy">
+          width="800" height="450" allowfullscreen="" loading="lazy">
 
         </iframe>
+        <p className="App-intro">
+          Nos encontramos en la 6ta avenida 8-60 Zona 1 de la Ciudad de Guatemala.
+
+        </p>
         <br />
         <br />
         <header className="App-header">
