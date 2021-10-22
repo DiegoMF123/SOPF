639d13a684f0cf52df837aef3a556694fd3958db
Diego Mijangos <diegisseb@gmail.com>
2021-10-21 23:59:52 -0600

    Hs22Sdds2dd

--- src/PaginaDeConctacto.js
+++ src/PaginaDeConctacto.js
@@ -27,7 +27,7 @@ class Appdos extends Component {
         <br />
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.78728754968!2d-90.45828788531881!3d14.668009579353928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x858999096fdbd161%3A0x170e7fbdd215149!2sGuatemala!5e0!3m2!1ses!2sgt!4v1634882041863!5m2!1ses!2sgt"
-          width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy">
+          width="600" height="450" allowfullscreen="" loading="lazy">
 
         </iframe>
         <br />
