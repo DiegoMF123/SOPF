91e1641e7eea2c9887fae8db77c1c29c3d9bafa8
Diego Mijangos <diegisseb@gmail.com>
2021-10-21 23:56:02 -0600

    Hs22Sdds2d

--- src/PaginaDeConctacto.js
+++ src/PaginaDeConctacto.js
@@ -23,7 +23,15 @@ class Appdos extends Component {
           Estas son las sedes
 
         </p>
+        <br />
+        <br />
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.78728754968!2d-90.45828788531881!3d14.668009579353928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x858999096fdbd161%3A0x170e7fbdd215149!2sGuatemala!5e0!3m2!1ses!2sgt!4v1634882041863!5m2!1ses!2sgt"
+          width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy">
 
+        </iframe>
+        <br />
+        <br />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Datos de contacto</h1>
@@ -44,10 +52,6 @@ class Appdos extends Component {
 
         </p>
         <br />
-        <Link to="/PaginaDeConctacto"><button>
-          Siguiente pàgina
-        </button>
-        </Link>
 
       </div>
     );
